0f45262a0a9be42e9b6cd6f113fbaa6bf166188f
Omar Diaa <omardiaa27@gmail.com>
2022-10-17 18:48:07 +0200

    updates

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-2.cnq3hzzvesl0.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
 export AWS_REGION=us-east-1
